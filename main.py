10d5489e07493a60afc216044d8b6e5b66f002da
idname <96003952+IDname-git@users.noreply.github.com>
2025-04-30 18:28:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-
 input_prompt = '>>> '
 print("      ::::::::::: :::::::::          ::::::::  :::    ::: :::::::::: ::::::::  :::    ::: ")
 print("         :+:     :+:    :+:        :+:    :+: :+:    :+: :+:       :+:    :+: :+:   :+:   ")
@@ -8,7 +7,7 @@ print("       +#+     +#++:++#+         +#+        +#++:++#++ +#++:++#  +#+     
 print("      +#+     +#+               +#+        +#+    +#+ +#+       +#+        +#+  +#+       ")
 print("     #+#     #+#               #+#    #+# #+#    #+# #+#       #+#    #+# #+#   #+#       ")
 print("########### ###                ########  ###    ### ########## ########  ###    ###       ")
-print("\n\n")
+print("")
 print("Made by IDname")
 print("Enter IPv4 or IPv6 address:")
 ip_address = input(input_prompt)
@@ -16,13 +15,13 @@ ip_address = input(input_prompt)
 api_url = 'https://ipleak.net/json/' + ip_address
 response = requests.get(api_url)
 
-print("\nInformation about " + ip_address + ":")
+print("Information about " + ip_address + ":")
 
 if response.status_code == 200:
     data = response.json()
 
     def safe_print(label, value):
-        print(f"{label}: {value if value is not None and value != '' else 'Not available'}")
+        print(f"{label}:  {value if value is not None and value != '' else'Not available'}")
 
     safe_print("ISP Name", data.get("isp_name"))
     safe_print("Country", data.get("country_name"))
@@ -35,6 +34,23 @@ if response.status_code == 200:
 else:
     print("Request failed with status code:", response.status_code)
     print("Response:", response.text)
+
+save_to_file = input("Do you want to save the information to a file? (y/n): ")
+if save_to_file == 'y':
+    with open('ip_info' + ip_address + '.txt', 'w') as file:
+        file.write(f"Information about {ip_address}:\n")
+        file.write(f"ISP Name: {data.get('isp_name')}\n")
+        file.write(f"Country: {data.get('country_name')}\n")
+        file.write(f"Region: {data.get('region_name')}\n")
+        file.write(f"City: {data.get('city_name')}\n")
+        file.write(f"Latitude: {data.get('latitude')}\n")
+        file.write(f"Longitude: {data.get('longitude')}\n")
+        file.write(f"Accuracy Radius: {data.get('accuracy_radius')}\n")
+        file.write(f"Timezone: {data.get('time_zone')}\n")
+        file.write("Ip checked with https://github.com/IDname-git/IP-Check")
+    print(f"Information saved to ip_info{ip_address}.txt")
+else:
+    print("Information not saved to file.")
 
 print('Press any key to exit...')
 input("3")
