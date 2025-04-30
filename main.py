8839c0a873bdeba501eb40c308682b2348bcf82c
idname <96003952+IDname-git@users.noreply.github.com>
2025-04-30 22:11:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,46 +13,82 @@ print("Enter IPv4 or IPv6 address:")
 ip_address = input(input_prompt)
 
 api_url = 'https://ipleak.net/json/' + ip_address
+api_url2 = 'https://reallyfreegeoip.org/json/' + ip_address
+api_url3 = 'https://ipwho.is/' + ip_address
 response = requests.get(api_url)
-
-print("Information about " + ip_address + ":")
-
 if response.status_code == 200:
     data = response.json()
-
-    def safe_print(label, value):
-        print(f"{label}:  {value if value is not None and value != '' else'Not available'}")
-
-    safe_print("ISP Name", data.get("isp_name"))
-    safe_print("Country", data.get("country_name"))
-    safe_print("Region", data.get("region_name"))
-    safe_print("City", data.get("city_name"))
-    safe_print("Latitude", data.get("latitude"))
-    safe_print("Longitude", data.get("longitude"))
-    safe_print("Accuracy Radius", data.get("accuracy_radius"))
-    safe_print("Timezone", data.get("time_zone"))
 else:
     print("Request failed with status code:", response.status_code)
     print("Response:", response.text)
+    data = {}
+
+response2 = requests.get(api_url2)
+if response2.status_code == 200:
+    data2 = response2.json()
+else:
+    print("Request failed with status code:", response2.status_code)
+    print("Response:", response2.text)
+    data2 = {}
+
+response3 = requests.get(api_url3)
+if response3.status_code == 200:
+    data3 = response3.json()
+else:
+    print("Request failed with status code:", response3.status_code)
+    print("Response:", response3.text)
+    data3 = {}
+
+def safe_format(*values):
+    return " | ".join([str(value if value is not None and value != '' else 'Not available') for value in values])
+
+print("Information about " + ip_address + ":")
+print(f"ISP Name: {safe_format(data.get('isp_name'), data2.get('isp_name'), data3.get('connection', {}).get('isp'))}")
+print(f"Country: {safe_format(data.get('country_name'), data2.get('country_name'), data3.get('country'))}")
+print(f"Region: {safe_format(data.get('region_name'), data2.get('region_name'), data3.get('region'))}")
+print(f"City: {safe_format(data.get('city_name'), data2.get('city'), data3.get('city'))}")
+print(f"Zip: {safe_format(data.get('zip_code'), data2.get('zip_code'), data3.get('postal'))}")
+print(f"Latitude: {safe_format(data.get('latitude'), data2.get('latitude'), data3.get('latitude'))}")
+print(f"Longitude: {safe_format(data.get('longitude'), data2.get('longitude'), data3.get('longitude'))}")
+print(f"Timezone: {safe_format(data.get('time_zone'), data2.get('time_zone'), data3.get('timezone', {}).get('id'))}")
+
+print("Extra information:")
+for label, value in [
+    ("Accuracy Radius", data.get('accuracy_radius')),
+    ("Metro Code", data2.get('metro_code')),
+    ("Domain", data3.get('connection', {}).get('domain')),
+    ("Organization", data3.get('connection', {}).get('org'))
+]:
+    print(f"{label}: {value if value is not None and value != '' else 'Not available'}")
 
 save_to_file = input("Do you want to save the information to a file? (y/n): ")
 if save_to_file == 'y':
     with open('ip_info' + ip_address + '.txt', 'w') as file:
-        file.write(f"Information about {ip_address}:\n")
-        file.write(f"ISP Name: {data.get('isp_name')}\n")
-        file.write(f"Country: {data.get('country_name')}\n")
-        file.write(f"Region: {data.get('region_name')}\n")
-        file.write(f"City: {data.get('city_name')}\n")
-        file.write(f"Latitude: {data.get('latitude')}\n")
-        file.write(f"Longitude: {data.get('longitude')}\n")
-        file.write(f"Accuracy Radius: {data.get('accuracy_radius')}\n")
-        file.write(f"Timezone: {data.get('time_zone')}\n")
-        file.write("Ip checked with https://github.com/IDname-git/IP-Check")
-    print(f"Information saved to ip_info{ip_address}.txt")
-else:
-    print("Information not saved to file.")
+        def safe_write_line(category, *values):
+            file.write(f"{category}: " + " | ".join([str(value if value is not None and value != '' else 'Not available') for value in values]) + "\n")
 
-print('Press any key to exit...')
-input("3")
-input("2")
-input("1")
+        file.write(f"Information about {ip_address}:\n")
+        safe_write_line("ISP Name", data.get('isp_name'), data2.get('isp_name'), data3.get('connection', {}).get('isp'))
+        safe_write_line("Country", data.get('country_name'), data2.get('country_name'), data3.get('country'))
+        safe_write_line("Region", data.get('region_name'), data2.get('region_name'), data3.get('region'))
+        safe_write_line("City", data.get('city_name'), data2.get('city'), data3.get('city'))
+        safe_write_line("Zip", data.get('zip_code'), data2.get('zip_code'), data3.get('postal'))
+        safe_write_line("Latitude", data.get('latitude'), data2.get('latitude'), data3.get('latitude'))
+        safe_write_line("Longitude", data.get('longitude'), data2.get('longitude'), data3.get('longitude'))
+        safe_write_line("Timezone", data.get('time_zone'), data2.get('time_zone'), data3.get('timezone', {}).get('id'))
+
+        file.write("Extra information:\n")
+        for label, value in [
+            ("Accuracy Radius", data.get('accuracy_radius')),
+            ("Metro Code", data2.get('metro_code')),
+            ("Domain", data3.get('connection', {}).get('domain')),
+            ("Organization", data3.get('connection', {}).get('org'))
+        ]:
+            file.write(f"{label}: {value if value is not None and value != '' else 'Not available'}\n")
+        file.write("File created with github.com/IDname-git/IP-Check\n")
+        print(f"Information saved to ip_info{ip_address}.txt")
+
+else:
+        print("Information not saved to file.")
+
+input('Press any key to exit...')
